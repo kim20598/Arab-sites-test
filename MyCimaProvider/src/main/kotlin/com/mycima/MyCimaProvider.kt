fc7ef85fa6fb800c6d17ab46fbdcac69e9199fb8
kim20598 <ki688494@gmail.com>
2025-10-10 16:22:04 +0100

    Update MyCimaProvider.kt

--- MyCimaProvider/src/main/kotlin/com/mycima/MyCimaProvider.kt
+++ MyCimaProvider/src/main/kotlin/com/mycima/MyCimaProvider.kt
@@ -11,7 +11,7 @@ import org.jsoup.nodes.Element
 
 class MyCima : MainAPI() {
     override var lang = "ar"
-    override var mainUrl = "https://wecima.tube"
+    override var mainUrl = "https://wecima.click"
     override var name = "MyCima"
     override val usesWebView = false
     override val hasMainPage = true
